73a22da5657ae183ab390fafe15205760738910e
jihoon <parkjh13579@hanyang.ac.kr>
2024-12-06 18:34:30 +0900

    png 파일을 24bit bmp 파일로 변환하는 메서드 추가 및 테스트 파일들 이름 정리

--- example_32bmp_usage.py
+++ example_32bmp_usage.py
@@ -2,30 +2,30 @@ from image_filter.custom_image_processor import CustomImageProcessor
 
 # 1. 32비트 BMP를 24비트 BMP로 변환
 processor = CustomImageProcessor()
-processor.convert_to_24bit("example_32bit.bmp", "example_24bit.bmp")
+processor.convert_to_24bit("32bit_example.bmp", "example_24bit.bmp")
 
 # 2. 24비트 BMP 로드
 processor.load("example_24bit.bmp")
 
 # 3. 흑백 변환
-processor.apply_grayscale("example_24bit_grayscale.bmp")
+processor.apply_grayscale("example_32_to_24_grayscale.bmp")
 
 # 4. 색상 반전
 processor.load("example_24bit.bmp")  # 원본 다시 로드
-processor.apply_invert_colors("example_24bit_inverted.bmp")
+processor.apply_invert_colors("example_32_to_24_inverted.bmp")
 
 # 5. 픽셀화
 processor.load("example_24bit.bmp")  # 원본 다시 로드
-processor.apply_pixelation(pixel_size=20, output_file="example_24bit_pixelated.bmp")
+processor.apply_pixelation(pixel_size=20, output_file="example_32_to_24_pixelated.bmp")
 
 # 6. 좌우반전
 processor.load("example_24bit.bmp")  # 원본 다시 로드
-processor.apply_flip_horizontal("example_24bit_flipped.bmp")
+processor.apply_flip_horizontal("example_32_to_24_flipped.bmp")
 
 #7. 밝기 올리기
 processor.load("example_24bit.bmp")  # 원본 다시 로드
 processor.apply_skin_brightness(
     brightness_factor=1.3,  # 밝기 증가 비율
     soften_intensity=25,    # 부드러움 추가 강도
-    output_file="example_24bit_brightness.bmp"  # 결과 저장 파일
+    output_file="example_32_to_24_brightness.bmp"  # 결과 저장 파일
 )
