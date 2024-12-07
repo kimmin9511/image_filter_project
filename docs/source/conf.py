4f79b58146b5242aad78ba8714d13f8d958e378f
jihoon <parkjh13579@hanyang.ac.kr>
2024-12-08 01:54:10 +0900

    d

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,11 +5,13 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('image_filter_project_fork/image_filter/'))
-project = 'image_filter_project_test'
+
+project = 'image_filter_project_oss'
 copyright = '2024, jihoon'
 author = 'jihoon'
 release = '0.1'
@@ -23,18 +25,19 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
     'sphinx_autodoc_typehints',
-    'myst_parser',  # Markdown 지원
 ]
 
-# HTML 테마
-html_theme = 'alabaster'
 
-# 정적 파일
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
 html_static_path = ['_static']
 html_logo = '_static/logo.png'
 html_favicon = '_static/favicon.ico'
 
-# 자동 요약 생성
 autosummary_generate = True
 autodoc_default_options = {
     'members': True,
@@ -42,5 +45,4 @@ autodoc_default_options = {
     'show-inheritance': False,
 }
 
-# 지원 파일 형식
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
--- docs/source/filters.rst
+++ docs/source/filters.rst
@@ -4,4 +4,6 @@ Filters
 Let me introduce our filters ;)
 
 .. automodule:: image_filter.custom_image_processor
-   :members: apply_blur, apply_flip_horizontal, apply_grayscale, apply_invert_colors, apply_neon_filter, apply_pixelation, apply_sepia_tone, apply_skin_brightness, apply_text_sticker
+   :members:
+   :exclude-members: auto_convert_to_24bit_bmp, convert_jpg_to_bmp, convert_png_to_bmp, convert_to_24bit, load, save
+   :show-inheritance: False
