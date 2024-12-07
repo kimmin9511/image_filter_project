7aa71ce4d9f0e4da16c58b7aeaf515d22440291f
jinihani <hanjun1910@gmail.com>
2024-12-07 19:45:27 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import os
 import sys
 
 # 프로젝트 루트 경로를 sys.path에 추가
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath('../../image_filter'))
 
 extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.viewcode']
 
